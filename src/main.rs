873c616a082a4410f02922b4c9388aecc3d55a77
lefty <geza.balogh@trayport.com>
2026-02-22 17:19:05 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::io::{self, BufRead};
+
+
+
+/*
+ * Complete the 'isAlphabeticPalindrome' function below.
+ *
+ * The function is expected to return a BOOLEAN.
+ * The function accepts STRING code as parameter.
+ */
+
+fn isAlphabeticPalindrome(code: &str) -> bool {
+
+	let code = code.chars().filter(|&c| c.is_alphabetic()).collect::<String>();
+	print!("{}", &code);
+	let reverse = code.chars().rev().collect::<String>();
+	print!("{}", &reverse);
+	println!("{}", &code == &reverse);
+	true
+
+}
+
+fn main() {
+	let stdin = io::stdin();
+	let mut stdin_iterator = stdin.lock().lines();
+
+	let code = stdin_iterator.next().unwrap().unwrap();
+
+	let result = isAlphabeticPalindrome(&code);
+
+	println!("{}", if result { 1 } else { 0 });
+}
